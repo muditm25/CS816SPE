875aab653e55541e56e3c3378659cb3dc21a17f1
Mudit <mudit.mittal@iiitb.org>
2019-01-17 09:49:20 +0530

    "Add module"

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Calculator {
+	public static void main(String args[]) {
+		float a,b,result;
+		char choice,ch;
+		Scanner sc=new Scanner(System.in);
+		do
+		{
+			System.out.println("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n 5. Exit");
+			choice=sc.next().charAt(0);
+			System.out.println("Enter two numbers:");
+			a=sc.nextFloat();
+			b=sc.nextFloat();
+			switch(choice)
+			{
+				case '1': result=a+b;
+					  break;
+				default : System.out.println("Invalid choice\n");
+			}
+			System.out.println("-----------------");
+		}while(choice!='5');
+	}
+}
